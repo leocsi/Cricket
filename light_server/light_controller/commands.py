372ef9ae0f7336d489c15c1c370a8be008307328
leocsi <horvath.leo2001@gmail.com>
2023-09-28 23:57:28 +0200

    Add a few more commands
    Fix imports and errors

--- light_server/light_controller/commands.py
+++ light_server/light_controller/commands.py
@@ -1,15 +1,25 @@
-from light_server.light_controller.assets import *
-from light_server.exceptions.light_server_exceptions import *
+from light_controller.assets import *
+from exceptions.light_server_exceptions import *
 
 class Commands:
-    def Commands(self):
+    def __init__(self):
         self.map = {
             "turn_on" : self.turn_on,
+            "turn_off": self.turn_off,
+            "toggle": self.toggle
         }
     def get_command_with_params(self, command):
         try:
             return self.map[command]()
         except KeyError:
             raise CommandNotFoundException(command)
-    def turn_on():
-        return 'turn_on'
+        
+    def turn_on(self):
+        return 'turn_on', None
+    
+    def turn_off(self):
+        return 'turn_off', None
+    
+    def toggle(self):
+        return 'toggle', None
+    
