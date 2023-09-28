f419c0a230fe9056d13e4c421822798477508892
leocsi <horvath.leo2001@gmail.com>
2023-09-28 23:56:45 +0200

    Add support for specific room control
    Fix imports and handle exceptions

--- light_server/light_controller/command_sender.py
+++ light_server/light_controller/command_sender.py
@@ -1,31 +1,49 @@
 import yeelight as yee
-from light_server.light_controller.commands import Commands
+from light_controller.commands import Commands
 
 class CommandSender:
-    def CommandSender(self):
+    def __init__(self):
+        self.all_devices = []
+        self.all_rooms = {}
         self.device_info = self.__load_devices()
-        self.all_devices = [yee.Bulb(ip) for ip in self.device_info.keys()]
         self.commands = Commands()
 
     def __load_devices(self) -> dict:
         with open("light_server/configured_devices.conf","r") as file:
             lines = file.readlines()
             all_bulbs = {}
+            current_bulb = ""
+            counter = 0
             for line in lines:
                 line = line.strip()
-                current_bulb = ""
                 if line[0] == "#":
                     current_bulb = line[1:]
                     all_bulbs.update({current_bulb: []})
+                    self.all_devices.append(yee.Bulb(current_bulb))
+                    counter = 0
                 else:
-                    all_bulbs.update({current_bulb: all_bulbs.get(current_bulb).append(line)})
+                    all_bulbs.get(current_bulb).append(line)
+                    if counter == 1:
+                        if line in self.all_rooms:
+                            self.all_rooms[line].append(self.all_devices[-1])
+                        else:
+                            self.all_rooms.update({line: [self.all_devices[-1]]})
+                counter += 1
             return all_bulbs
 
 
     def send(self, command: str) -> None:
-        parsed_command, arguments = self.commands.get_command_with_params(command)
+        split_command = command.split()
         
-        for device in self.all_devices:
+        if split_command[0] in self.all_rooms:
+            parsed_command, arguments = self.commands.get_command_with_params(split_command[1])
+            devices = self.all_rooms[split_command[0]]
+        
+        else:
+            parsed_command, arguments = self.commands.get_command_with_params(command)
+            devices = self.all_devices
+
+        for device in devices:
             device.get_properties
             function = device.__getattribute__(parsed_command)
             result = function(arguments)
