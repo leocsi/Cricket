994fb72f2c3f4dc4081fc263b3d8017bdcd8cd80
leocsi <horvath.leo2001@gmail.com>
2023-11-20 16:46:27 +0100

    Add peer certificate checking functionality
    Ensure required certificates are present

--- light_server/security/security_manager.py
+++ light_server/security/security_manager.py
@@ -1,28 +1,82 @@
 from security.security_utils import *
 from os import path
+from OpenSSL.SSL import Connection
  
 CERT_PATH = "light_server/security/certs/"
-FILES = ["CA.cert","CA.pkey","server.cert","server.pkey","client.pkey","client.cert"]
+FILES = ["server.cert","server.pkey","client.pkey","client.cert", "crl"]
 
 class Security:
-    def missing_certs(self):
+    """Class responsible for ensuring necessary certificates are present.
+    Assumes the existence of a local CA authority."""
+    def missing_certs(self) -> bool:
+        """This function checks if any of the necessary files are missing.
+        
+        :returns: A boolean indicating whether there are missing files"""
         for file in FILES:
             if not path.isfile(CERT_PATH + file):
                 print("Some certificates were missing or incomplete.")
                 return True
         return False
-            
-    def cert_generator(self):
-        print("Generating certificates...")
-        generate_ca()
-        print("Root CA generated.")
+   
+    def cert_generator(self) -> None:
+        """This method generates the required certificate files for the application to run. 
+        """
 
-        generate_cert("server","Server Certificate")
-        print("Server certificate generated")
+        print("Generating CRL...")
+        generate_crl()
 
-        generate_cert("client","Client Certificate")
-        print("Client certificate generated")
+        generate_cert("server",CN="Server Certificate")
+        print("Server certificate generated!")
 
+        if input("Would you like to create a client certificate? (Y/N)") == "Y":
+            certname = input("New certificate Common Name: ")
+            certfile = input("New certificate file name: ")
+            self.new_client(certfile, certname)
 
-def verify_cert(a,b,c,d,e):
-    return True
+    def new_client(self, filename:str, name:str) -> None:
+        """Wrapper method to generate a new client certificate.
+        
+        :param filename: name of the file to place the new certificate in
+        :param name: Common Name of the new certificate
+        """
+
+        generate_cert(filename, CN=name)
+        print("Client certificate generated!")
+
+def verify_cert(conn: Connection, cert: crypto.X509, error: int, depth: int, ret: int) -> bool:
+    """This method is a callback for socket connection, it performs a hardened verification process, checking for certificate revocations, and strict x509 requirements
+
+    :param conn: connection this check is being performed on
+    :param cert: certificate being checked
+    :param error: error code if it happened
+    :param depth: how deep the current certificate in the chain is
+    :param ret: return code
+    :returns: A boolean indicating whether verification was successful
+    """
+    try:
+        store = crypto.X509Store()
+        
+        crl = crypto.load_crl(crypto.FILETYPE_PEM, open(f"light_server/security/certs/crl",'rb').read())
+        store.add_crl(crl)
+        
+        if hex(cert.get_serial_number())[2:] in [str(revoked.get_serial(),"utf-8").lower() for revoked in crl.get_revoked()]:
+            return False
+  
+        inter = crypto.load_certificate(crypto.FILETYPE_PEM, open("light_server/security/certs/inter.cert","rb").read())
+        store.add_cert(inter)
+        
+        ca = crypto.load_certificate(crypto.FILETYPE_PEM, open("light_server/security/certs/ca.cert","rb").read())
+        store.add_cert(ca)
+
+        store.set_flags(crypto.X509StoreFlags.CRL_CHECK_ALL | crypto.X509StoreFlags.X509_STRICT)
+
+        storectx = crypto.X509StoreContext(store, cert)
+        storectx.verify_certificate()
+        return True
+    except crypto.X509StoreContextError as ce:
+        print(ce)
+        return False
+    except Exception as e:
+        print(e)
+        return False
+    
--- light_server/security/security_utils.py
+++ light_server/security/security_utils.py
@@ -1,53 +1,123 @@
+"""Module that supplies security related features, mainly 
+related to TLS encryption. Methods assume the existence of a local CA."""
+
+
 from OpenSSL import crypto
-import os
-import getpass
+import getpass,random
+from datetime import datetime
 
-def generate_ca():
-    key = new_key_pair(2048)
-    careq = new_cert_request(key, CN="Cricket Root Certificate Authority")
-    cacert = new_cert(careq, careq, key, 0, 0, 60*60*24*365*2)
-    path = "light_server/security/certs"
-    if not os.path.exists(path):
-        os.makedirs(path)
-    keyfile = open(path+"/CA.pkey","w")
-    keyfile.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key, cipher="aes-256-cbc", passphrase=get_passwd).decode("utf-8"))
-    certfile = open(path+'/CA.cert', 'w')
-    certfile.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cacert).decode("utf-8"))
+CERT_PATH = "light_server/security/certs/"
+"""Directory where certificates for the server are stored"""
 
-def generate_cert(file_name, cert_name):
-    key = new_key_pair(2048)
-    req = new_cert_request(key, CN=cert_name)
-    ca = crypto.load_certificate(crypto.FILETYPE_PEM, open("light_server/security/certs/CA.cert","rb").read())
-    cakey = crypto.load_privatekey(crypto.FILETYPE_PEM, open("light_server/security/certs/CA.pkey","rb").read(),passphrase=get_passwd)
-    cert = new_cert(req, ca, cakey, 0, 0, 60*60*24*365)
-    open(f"light_server/security/certs/{file_name}.cert",'w').write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode("utf-8"))
-    open(f"light_server/security/certs/{file_name}.pkey",'w').write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key, cipher="aes-256-cbc", passphrase=get_passwd).decode("utf-8"))
 
-def generate_crl():
-    pass
+def generate_cert(file_name: str, **subject: "dict[str:str]") -> None:
+    """Generates a new certificate according to given attributes, assumes the existence of a local CA
 
-def revoke_cert():
-    pass
+    :param file_name: name of the file to dump the new certificate to 
+    :param subject: subject fields of the new certificate as key-value pairs, key has to be a valid ``X509Name`` field, such as `C` or `countryName`   
+    """
+    key = new_private_key(2048)
 
-def new_key_pair(size: int):
+    cakey = crypto.load_privatekey(crypto.FILETYPE_PEM, open(f"{CERT_PATH}inter.pkey","rb").read(),passphrase=get_passwd)
+    ca = crypto.load_certificate(crypto.FILETYPE_PEM, open(f"{CERT_PATH}inter.cert","rb").read())
+    ca_subject = ca.get_subject().get_components()
+    cert_subject = {str(attr,"utf-8"):str(value,"utf-8") for (attr,value) in ca_subject}
+    for (attr,value) in subject.items():
+        cert_subject.update({attr : value})
+
+    req = new_cert_request(key, **cert_subject)
+    cert = new_cert(req, ca, cakey, random.randint(1, 50000), 0, 60*60*24*365)
+    
+    open(f"{CERT_PATH}{file_name}.cert",'w').write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode("utf-8"))
+    open(f"{CERT_PATH}{file_name}.pkey",'w').write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key, cipher="aes-256-cbc", passphrase=set_passwd).decode("utf-8"))
+
+
+def generate_crl() -> None:
+    """Generates a certificate revocation list, with a list of given certificates.
+
+    :param certs: list of certificates to add to the list initally, can be None for empty crl
+    """
+    crl = crypto.CRL()
+    crl.set_lastUpdate(bytes(get_asn1(), "utf-8"))
+
+    ca = crypto.load_certificate(crypto.FILETYPE_PEM, open(f"{CERT_PATH}inter.cert","rb").read())
+    cakey = crypto.load_privatekey(crypto.FILETYPE_PEM, open(f"{CERT_PATH}inter.pkey","rb").read(),passphrase=get_passwd)
+
+    crl.sign(ca, cakey, b"sha512")
+    open(f"{CERT_PATH}crl",'w').write(crypto.dump_crl(crypto.FILETYPE_PEM,crl).decode("utf-8"))
+
+
+def revoke_cert(cert:crypto.X509, reason: bytes = b"unspecified") -> None:
+    """Revokes a certificate, adding it to the CRL.
+
+    :param cert: certificate to revoke
+    :param reason: reason of revocation
+    """
+
+    crl = crypto.load_crl(crypto.FILETYPE_PEM, open(f"{CERT_PATH}crl",'rb').read())
+    ca = crypto.load_certificate(crypto.FILETYPE_PEM, open(f"{CERT_PATH}inter.cert","rb").read())
+    cakey = crypto.load_privatekey(crypto.FILETYPE_PEM, open(f"{CERT_PATH}inter.pkey","rb").read(),passphrase=get_passwd)
+
+    revoked = crypto.Revoked()
+    revoked.set_reason(reason)
+    serial_hex = bytes(hex(cert.get_serial_number()),"ascii")[2:]
+    revoked.set_serial(serial_hex)
+    revoked.set_rev_date(bytes(get_asn1(), "utf-8"))
+    
+    crl.add_revoked(revoked)
+    crl.set_lastUpdate(bytes(get_asn1(), "utf-8"))
+
+    crl.sign(ca,cakey,b"sha512")
+
+    open(f"{CERT_PATH}crl",'w').write(crypto.dump_crl(crypto.FILETYPE_PEM, crl).decode("utf-8"))    
+
+
+def new_private_key(size: int=2048) -> crypto.PKey:
+    """Generate new RSA private key
+
+    :param size: size of new key in bits (at least 2048)
+    :returns: The generated key
+    """
+    if size < 2048:
+        print("Key too weak! Please specify at least 2048 bits for key size.")
+        return None
+    
     key_pair = crypto.PKey()
     key_pair.generate_key(crypto.TYPE_RSA, size)
     return key_pair
 
-def new_cert_request(pkey, digest="sha512", **name):
+
+def new_cert_request(pkey: crypto.PKey, digest: str="sha512", **subject:"dict[str:str]") -> crypto.X509Name:
+    """Generates a new certificate according to given attributes, assumes the existence of a local CA
+
+    :param pkey: the key to associate with the request 
+    :param digest: the hashing algorithm to be used for signing. defaults to sha512
+    :param subject: subject fields of the new certificate as key-value pairs, key has to be a valid ``X509Name`` field, such as `C` or `countryName`   
+    """
     req = crypto.X509Req()
     subj = req.get_subject()
-    for (key, value) in name.items():
+    for (key, value) in subject.items():
         setattr(subj, key, value)
-
     req.set_pubkey(pkey)
     req.sign(pkey, digest)
-    
     return req
 
-def new_cert(req: crypto.X509Req, issuerCert: crypto.X509, issuerKey: crypto.PKey, serial, notBefore, notAfter, digest="sha512"):
+
+def new_cert(req: crypto.X509Req, issuerCert: crypto.X509, issuerKey: crypto.PKey, serial: int, notBefore: int, notAfter: int, digest: str="sha512"):
+    """Generate new X509v3 certificate.
+
+    :param req: Certificate request to use
+    :param issuerCert: CA issuer that should sign the request
+    :param issuerKey: Private key of CA
+    :param serial: Serial number of new cert
+    :param notBefore: Start of validity date for certificate (In seconds, starting from current time)
+    :param notAfter: End of validity date for certificate (In seconds, starting from current time)
+    :param digest: the hashing algorithm to be used for signing. defaults to sha512
+    :returns: ``X509`` certificate object
+    """
     cert = crypto.X509()
     cert.set_serial_number(serial)
+    cert.set_version(2)
     
     cert.gmtime_adj_notBefore(notBefore)
     cert.gmtime_adj_notAfter(notAfter)
@@ -56,12 +126,49 @@ def new_cert(req: crypto.X509Req, issuerCert: crypto.X509, issuerKey: crypto.PKe
     
     cert.set_subject(req.get_subject())
     cert.set_pubkey(req.get_pubkey())
-    
+
+    extensions = []
+    extensions.append(crypto.X509Extension(b"subjectKeyIdentifier", False, b"hash", subject=cert))
+    extensions.append(crypto.X509Extension(b'authorityKeyIdentifier', False, b"keyid:always,issuer:always",issuer=issuerCert))
+    extensions.append(crypto.X509Extension(b"keyUsage", True, b"digitalSignature,nonRepudiation,keyEncipherment"))
+    extensions.append(crypto.X509Extension(b"extendedKeyUsage",False, b"clientAuth,emailProtection"))
+    cert.add_extensions(extensions)
+
     cert.sign(issuerKey, digest)
     return cert
 
-def get_passwd(a):
-    passwd = getpass.getpass("Enter a password to encrypt private key (Make sure to remember this!): ")
+
+def set_passwd(x) -> bytes:
+    """Callback function to set password for newly generated private keys.
+
+    :returns: inputted password in bytes format
+    """
+    while True:
+        passwd = getpass.getpass("Enter a password to encrypt private key (Make sure to remember this!): ")
+        passwd_confirm = getpass.getpass("Confirm password: ")
+        if passwd == passwd_confirm:
+            break
+        else:
+            print("Passwords do not match!")
+
     return bytes(passwd,"utf-8")
 
-      
+
+def get_passwd(x) -> bytes:
+    """Callback function to get password to unlock a private key.
+
+    :returns: inputted password in bytes format
+    """
+
+    passwd = getpass.getpass("Enter password for private key: ")
+    return bytes(passwd,"utf-8")
+
+
+def get_asn1(time: datetime=datetime.now().utcnow()) -> str:
+    """ Utility function to convert given time to ASN1 format: YYYYMMDDHHMMSSZ
+    
+    :param time: Given timestamp to parse
+    :returns: ASN1 formatted time
+    """
+    time = f"{time.year}{time.month:02d}{time.day:02d}{time.hour:02d}{time.minute:02d}{time.second:02d}Z"
+    return time
