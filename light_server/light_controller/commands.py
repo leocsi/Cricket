17d08a223f8ab31977aa02461b94080adc9a3645
leo <horvath.leo2001@gmail.com>
2023-12-05 18:24:40 +0100

    Rewrite command storage logic

--- light_server/light_controller/commands.py
+++ light_server/light_controller/commands.py
@@ -5,44 +5,79 @@ from yeelight import Flow, RGBTransition
 from yeelight.flows import lsd, police
 
 class Commands:
-    def __init__(self):
-        self.map = {
-            "turn_on" : self.turn_on,
-            "turn_off": self.turn_off,
-            "toggle": self.toggle,
-            "pulse": self.pulse,
-            "lsd": self.lsd,
-            "damage": self.damage,
-            "police": self.police,
-            'aurora_borealis': self.aurora_borealis
-        }
-        
-    def get_command_with_params(self, command):
+
+    @staticmethod
+    def turn_on():
+        return 'turn_on:0:0', None
+    
+    @staticmethod
+    def turn_off():
+        return 'turn_off:0:0', None
+    
+    @staticmethod
+    def toggle():
+        return 'toggle:0:0', None
+    
+    @staticmethod
+    def pulse():
+        return 'start_flow:0:0', Flow(count=1, transitions=pulse(255, 0, 0, 50))
+    
+    @staticmethod
+    def lsd():
+        return 'start_flow:0:0', lsd()
+    
+    @staticmethod
+    def damage():
+        return 'start_flow:0:0', Flow(count=1, transitions=pulse(255, 0, 0, 50)+[RGBTransition(255,0,0,50,10),RGBTransition(255,0,0,200,100)])
+    
+    @staticmethod
+    def police():
+        return 'start_flow:0:0', police()
+    
+    @staticmethod
+    def aurora_borealis():
+        return 'start_flow:1:0', aurora_borealis()
+
+    MAP = {
+        "turn_on:" : turn_on,
+        "turn_off": turn_off,
+        "toggle": toggle,
+        "pulse": pulse,
+        "lsd": lsd,
+        "damage": damage,
+        "police": police,
+        'aurora_borealis': aurora_borealis
+    }
+
+    @staticmethod
+    def get_command_with_params(command):
         try:
-            return self.map[command]()
+            return Commands.MAP[command]()
         except KeyError:
             raise CommandNotFoundException(command)
-        
-    def turn_on(self):
-        return 'turn_on', None
+
+class CommandContext:
+    def __init__(self, name):
+        self.name = name
+        self.command, self.flow = Commands.get_command_with_params(name)
+
+        self.command = self.parse_execution_mode(self.command)
     
-    def turn_off(self):
-        return 'turn_off', None
+    def parse_execution_mode(self, command):
+        command_components = command.split(':')
+        command_name = command_components[0]
+        command_modes = command_components[1:]
+
+        attributes = ['trueRandom', 'stateful']
+        for i, attr in enumerate(attributes):
+            if i >= len(command_modes):
+                self.__setattr__(attr, False)
+            else:
+                self.__setattr__(attr, bool(int(command_modes[i])))
+
+        return command_name
     
-    def toggle(self):
-        return 'toggle', None
-    
-    def pulse(self):
-        return 'start_flow', Flow(count=1, transitions=pulse(255, 0, 0, 50))
-    
-    def lsd(self):
-        return 'start_flow', lsd()
-    
-    def damage(self):
-        return 'start_flow', Flow(count=1, transitions=pulse(255, 0, 0, 50)+[RGBTransition(255,0,0,50,10),RGBTransition(255,0,0,200,100)])
-    
-    def police(self):
-        return 'start_flow', police()
-    
-    def aurora_borealis(self):
-        return 'start_flow', aurora_borealis()
+    def new_flow(self):
+        if not self.trueRandom:
+            raise Exception()
+        _, self.flow = Commands.get_command_with_params(self.name)
